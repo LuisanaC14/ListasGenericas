af222c9c02999beff33a7c0838c848791786d225
H3rnan379 <nandococha1@gmail.com>
2025-02-05 23:00:25 -0500

    Update Form1.cs
    Modificado

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,17 @@ namespace GestionCafeteriaWF
 
         private void btnProcesarPedido_Click(object sender, EventArgs e)
         {
+        if (pedidos.Count > 0)
+            {
+                Cafeteria procesado = pedidos[0];
+                pedidos.RemoveAt(0);
+                MessageBox.Show($"Pedido de {procesado.Cliente} procesado.");
+                ActualizarListaPedidos();
+            }
+            else
+            {
+                MessageBox.Show("No hay pedidos para procesar.");
+            }
 
         }
 
